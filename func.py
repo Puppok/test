6a5cd0475cf29dfa00bde421af9a980b1abde7bb
Puppok <kravchenko19971808@gmail.com>
2025-04-10 18:21:21 +0800

    puk

--- func.py
+++ func.py
@@ -2,3 +2,11 @@ def compare(a, b):
     if a > b: print(f'{a} больше')
     elif a < b : print(f'{b} больше')
     else: print('Числа равны')
+    # sdhsdh
+    # sortedsdh
+    # sdh
+    # sdh
+    # sdh
+    # sdg
+    # sdh
+    # sdh
